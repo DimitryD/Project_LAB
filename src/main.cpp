87e0a0bd329f36a3aa7720c86ff94ce4634e5bc9
Khadija Saoulajane <k.saoulajane@imero.de>
2022-01-25 21:31:22 +0100

    adding comments

--- src/main.cpp
+++ src/main.cpp
@@ -38,44 +38,56 @@ void loop(){
   delay(1000);
 }
 
+// this is a callback function to process the received message.
 void messageHandler(String &topic, String &payload) {
     // Serial.println("incoming: " + topic + " - " + payload);
     if (&payload == nullptr || payload.equals("null") || payload == NULL) return;    
+    // if the topic is the rover then updateRover with new payload 
+    //(message received from AWS as follows {"21": [[126, 43], 276]})
     if (topic.equals("greengrass/group10rover")) roverLogic.updateRover(payload);
     else roverLogic.updateTarget(payload);
 }
 
+// ------------------- TASK ONE responsible for keeping aws connected  ----------------------------
 void taskOne( void * parameter )
 {
   Serial.println("AWS taskOne");
   myawsclass awsobject = myawsclass();
+  // we connect to wifi and AWS
   awsobject.connectAWS();
+  // to receive messages we need to create a callback function
   awsobject.messageHandler(messageHandler);
+
   //example of a task that executes for some time and then is deleted
   for(;; ) {
       bool b = awsobject.stayConnected();
       if (!b) awsobject.connectAWS();
-      
       vTaskDelay(5); //this pauses the task, so others can execute
-    
   }
   Serial.println("Ending task: 1"); //should not reach this point.
   vTaskDelete( NULL );
 }
  
+
+// ------------------- TASK TWO used to setup sensor, motors and calling method to reach target  ----------------------------
 void taskTwo(void * parameter) {
   Serial.println("Logic taskTwo");
 
   Serial.println("init sensor");
   sclass sensorobject = sclass();
+  // setup sensor object
   sensorobject.SETUP();
+  // set sensor values
   roverLogic.setSensor(sensorobject);
 
   Serial.println("init motor");
   mclass motorobject = mclass();
+  // setup motor object
   motorobject.SETUP();
+  //set motor object and mobe it forward
   roverLogic.setMotor(motorobject);
 
+  //main function to reach target while avoiding obstacles
   roverLogic.reachTarget();
 
    Serial.println("Ending task 2"); //should not reach this point.
